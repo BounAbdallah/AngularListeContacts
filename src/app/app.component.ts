9b1d773f5e3df520160dd20c3879c5c3dae8add7
BounAbdallah <bigmabo61345@gmail.com>
2024-07-29 15:57:50 +0000

    Resolutionde bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
-import { IndexComponent } from "./post/index/index.component";
-import { CreateComponent } from "./post/create/create.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true, // Déclare le composant comme autonome
-  imports: [RouterOutlet, LoginComponent, IndexComponent, CreateComponent], // Importation des modules nécessaires pour le routage
+  imports: [RouterOutlet, LoginComponent ], // Importation des modules nécessaires pour le routage
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,6 @@
 //src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-import { IndexComponent } from './post/index/index.component';
-
-import { ViewComponent } from './post/view/view.component';
-
-import { CreateComponent } from './post/create/create.component';
-
-import { EditComponent } from './post/edit/edit.component';
 
   
 
@@ -15,12 +8,6 @@ export const routes: Routes = [
 
       { path: 'post', redirectTo: 'post/index', pathMatch: 'full'},
 
-      { path: 'post/index', component: IndexComponent },
-
-      { path: 'post/:postId/view', component: ViewComponent },
-
-      { path: 'post/create', component: CreateComponent },
-
-      { path: 'post/:postId/edit', component: EditComponent } 
+      
 
   ];
